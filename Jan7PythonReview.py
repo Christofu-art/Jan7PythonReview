001c44b295cd077ddb025a8423d3a18581b1a96e
Chrissy Wake UP, I don like this, CHRISSY wake up! <universecrusader16@gmail.com>
2025-01-07 15:28:48 -0700

    Add files via upload

---
+++ Jan7PythonReview.py
@@ -0,0 +1,23 @@
+#Chris Sullivan
+line = int(input("How many lines have you cleared in Tetris: ")) 
+if line < 100: #Dr. Mo
+    print("Novice")
+elif line < 200: #Dr. Mo
+    print("Intermediate")
+elif line < 300: #Dr. Mo
+    print("Advance")
+else:
+    print("Expert")
+
+
+
+
+
+
+for i in range(2, 80, 10): #Chris Sullivan
+    print(i, end = " ")
+print()
+
+for i in range(100, 10, -5): #Chris Sullivan
+    print(i, end = " ")
+print()
